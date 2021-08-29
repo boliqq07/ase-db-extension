7c097e34633792dbadf3173bacf976fe89505d75
changxin W <986798607@qq.com>
2021-08-29 23:19:12 +0800

    add file

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
 
-    name='ase_db_extendion',
+    name='ase_db_extension',
     version='0.0.01',
     keywords=['database'," structure"],
     description='database for ase.db.',
